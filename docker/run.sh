2f6cd354e0860a0367588e52ac70b43a9b0423d0
Emil Södergren <EmilSodergren@users.noreply.github.com>
2019-02-11 09:03:42 +0100

    First commit of scripts building, jdk binaries

---
+++ docker/run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -e
+
+if [[ "$VERSION" = "jdk8u" ]]; then
+    CONFIG_PARAMS=""
+elif [[ "$VERSION" = "jdk11" ]]; then
+    TIME=`date '+%F-%H-%M-%S'`
+    CONFIG_PARAMS="--with-version-opt=$TIME --disable-warnings-as-errors"
+elif [[ "$VERSION" = "jdk12" ]]; then
+    TIME=`date '+%F-%H-%M-%S'`
+    CONFIG_PARAMS="--with-version-opt=$TIME --disable-warnings-as-errors"
+else
+    echo "The version $VERSION is not in the run.sh script."
+    exit 1
+fi
+
+cd /build
+
+bash configure -q $CONFIG_PARAMS --prefix=/install/usr/local/
+if [[ "$1" = "--clean" ]]; then
+    make clean
+fi
+make images
+make install
+
+cd /install
+zip jdk_$VERSION.zip usr/ -r
+mv jdk_$VERSION.zip /build/
---
+++ run.py
@@ -0,0 +1,26 @@
+from os.path import exists, join
+import os
+from sys import exit
+from subprocess import call, Popen
+from argparse import ArgumentParser
+
+parser = ArgumentParser(description='Setup the machine')
+
+parser.add_argument('source_version', help='Version of JDK to build, needs to correspond to the folder name')
+parser.add_argument('bootstrap_jdk_package', help='Which version to use as bootstrap JDK, by spec this should most of the time be JDK version N-1')
+parser.add_argument('--clean', '-c', action='store_true')
+args = parser.parse_args()
+
+v = args.source_version
+
+if not exists(v):
+    print('No such version exists')
+    exit(1)
+
+docker_build_name = "jdk_builder_"+v
+p = Popen(["docker", "build", "--build-arg", "JDK_VERSION="+args.bootstrap_jdk_package, "-t", docker_build_name, "."], cwd="./docker")
+p.wait()
+clean = ""
+if (args.clean):
+    clean = "\ --clean"
+call(["docker","run","-t","-v",join(os.getcwd(),v)+":/build","-e", "VERSION="+v, docker_build_name, "/run.sh", "--clean"])

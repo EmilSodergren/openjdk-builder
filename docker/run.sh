302389e6d9ab90f21bf0f0de1083f5fa99d13978
Emil Södergren <EmilSodergren@users.noreply.github.com>
2019-02-12 14:52:27 +0100

    Changed install dir and added some parameters

--- docker/run.sh
+++ docker/run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -e
 
 if [[ "$VERSION" = "jdk8u" ]]; then
-    CONFIG_PARAMS=""
+    CONFIG_PARAMS="--with-milestone=emil --disable-zip-debug-info"
     PACKAGE_NAME="openjdk-1.8.0-emil"
 elif [[ "$VERSION" = "jdk11" ]]; then
     TIME=`date '+%F-%H-%M-%S'`
-    CONFIG_PARAMS="--with-version-opt=$TIME --disable-warnings-as-errors"
+    CONFIG_PARAMS="--with-version-opt=$TIME --with-version-pre=emil --disable-warnings-as-errors"
     PACKAGE_NAME="openjdk-11-emil"
 elif [[ "$VERSION" = "jdk12" ]]; then
     TIME=`date '+%F-%H-%M-%S'`
-    CONFIG_PARAMS="--with-version-opt=$TIME --disable-warnings-as-errors"
+    CONFIG_PARAMS="--with-version-opt=$TIME --with-version-pre=emil --disable-warnings-as-errors"
     PACKAGE_NAME="openjdk-12-emil"
 else
     echo "The version $VERSION is not in the run.sh script."
@@ -19,7 +19,7 @@ fi
 mkdir $PACKAGE_NAME
 
 pushd /build > /dev/null
-bash configure -q $CONFIG_PARAMS --with-native-debug-symbols=none --disable-zip-debug-info --prefix=/$PACKAGE_NAME/usr/lib/
+bash configure -q $CONFIG_PARAMS --with-native-debug-symbols=none --prefix=/$PACKAGE_NAME/usr/lib/
 if [[ "$1" = "--clean" ]]; then
     make clean
 fi

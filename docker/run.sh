16418fdf6eb410c5a4488ae41723898612c07adb
Emil Sodergren <EmilSodergren@users.noreply.github.com>
2020-09-10 14:13:15 +0200

    Moved the testing code to an if block

--- docker/run.sh
+++ docker/run.sh
@@ -28,6 +28,18 @@ elif [[ "$VERSION" = "jdk12" ]]; then
     TIME=`date '+%F-%H-%M-%S'`
     CONFIG_PARAMS="--with-version-opt=$TIME --with-version-pre=emil --disable-warnings-as-errors"
     PACKAGE_NAME="openjdk-12-emil"
+elif [[ "$VERSION" = "jdk13" ]]; then
+    TIME=`date '+%F-%H-%M-%S'`
+    CONFIG_PARAMS="--with-version-opt=$TIME --with-version-pre=emil --disable-warnings-as-errors"
+    PACKAGE_NAME="openjdk-13-emil"
+elif [[ "$VERSION" = "jdk14" ]]; then
+    TIME=`date '+%F-%H-%M-%S'`
+    CONFIG_PARAMS="--with-version-opt=$TIME --with-version-pre=emil --disable-warnings-as-errors"
+    PACKAGE_NAME="openjdk-14-emil"
+elif [[ "$VERSION" = "jdk15" ]]; then
+    TIME=`date '+%F-%H-%M-%S'`
+    CONFIG_PARAMS="--with-version-opt=$TIME --with-version-pre=emil --disable-warnings-as-errors"
+    PACKAGE_NAME="openjdk-15-emil"
 else
     echo "The version $VERSION is not in the run.sh script."
     exit 1
@@ -56,59 +68,58 @@ sed -i "s#{package_name}#$PACKAGE_NAME#g" `find $PACKAGE_NAME/DEBIAN -type f`
 
 dpkg-deb --build $PACKAGE_NAME
 
+if [[ $NO_TEST != 1 ]]; then
+
+    # Test the package
+    echo ""
+    echo "-----"
+    echo "Install and verify $PACKAGE_NAME"
+    echo "-----"
+    echo ""
+
+    cd /packages
+
+    apt install -y ./$PACKAGE_NAME.deb &> /tmp/install.log
+    if [[ "$?" != "0" ]]; then
+        echo "Error:"
+        cat /tmp/install.log
+        exit 1
+    else
+        echo "Package $PACKAGE_NAME.deb installs successfully"
+        echo "The current java version is:"
+        java -version
+    fi
+
+    if grep update-alternatives /tmp/install.log &> /dev/null
+    then
+        echo "Package $PACKAGE_NAME.deb seems to be correctly configured."
+    else
+        echo "Package $PACKAGE_NAME.deb does not configure correctly. See \"postinst\" script in debconf folder."
+        exit 1
+    fi
+
+    echo ""
+    echo "-----"
+    echo ""
+
+    apt purge -y $PACKAGE_NAME &> /tmp/uninstall.log
+    if [[ "$?" != "0" ]]; then
+        echo "Error:"
+        cat /tmp/uninstall.log
+        exit 1
+    else
+        echo "Package $PACKAGE_NAME.deb uninstalls successfully"
+        echo "The current java version is:"
+        java -version
+    fi
+
+    if grep update-alternatives /tmp/uninstall.log &> /dev/null
+    then
+        echo "Package $PACKAGE_NAME.deb seems to be correctly de-configured."
+    else
+        echo "Package $PACKAGE_NAME.deb is not deconfigured corectly. See \"prerm\" script in debconf folder."
+        exit 1
+    fi
+fi
+
 mv $PACKAGE_NAME.deb /packages/
-
-if [[ $NO_TEST = 1 ]]; then
-    exit 0
-fi
-
-# Test the package
-echo ""
-echo "-----"
-echo "Install and verify $PACKAGE_NAME"
-echo "-----"
-echo ""
-
-cd /packages
-
-apt install -y ./$PACKAGE_NAME.deb &> /tmp/install.log
-if [[ "$?" != "0" ]]; then
-    echo "Error:"
-    cat /tmp/install.log
-    exit 1
-else
-    echo "Package $PACKAGE_NAME.deb installs successfully"
-    echo "The current java version is:"
-    java -version
-fi
-
-if grep update-alternatives /tmp/install.log &> /dev/null
-then
-    echo "Package $PACKAGE_NAME.deb seems to be correctly configured."
-else
-    echo "Package $PACKAGE_NAME.deb does not configure correctly. See \"postinst\" script in debconf folder."
-    exit 1
-fi
-
-echo ""
-echo "-----"
-echo ""
-
-apt purge -y $PACKAGE_NAME &> /tmp/uninstall.log
-if [[ "$?" != "0" ]]; then
-    echo "Error:"
-    cat /tmp/uninstall.log
-    exit 1
-else
-    echo "Package $PACKAGE_NAME.deb uninstalls successfully"
-    echo "The current java version is:"
-    java -version
-fi
-
-if grep update-alternatives /tmp/uninstall.log &> /dev/null
-then
-    echo "Package $PACKAGE_NAME.deb seems to be correctly de-configured."
-else
-    echo "Package $PACKAGE_NAME.deb is not deconfigured corectly. See \"prerm\" script in debconf folder."
-    exit 1
-fi
